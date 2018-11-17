a0791e1aee5cb669fee7f1fddcede45de1d528fb
holisticqa <bart.kasik@gmail.com>
2018-11-17 21:11:50 +0100

    add gatsbyjs

---
+++ blog/src/components/Bio.js
@@ -0,0 +1,47 @@
+
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './avatar-2-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+        
+      >
+        <img
+          src={profilePic}
+          alt={`Kasia and Bart`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Written by <strong>Kasia Balcerzak</strong> and <strong> Bart Szulc</strong> who live in Gdansk and works at Spartez / Atlassian making cool stuff.{' '}
+          <div>
+          <div id="bartTwitter"><a href="https://twitter.com/BartSzulc">
+            Follow Bart on Twitter
+          </a></div> 
+          <div id="kasiaTitter"><a href="https://twitter.com/kasia_balcerzak">
+            and Kasia as well.
+          </a></div>
+          </div>
+          </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ blog/src/pages/hello-kasik/index.md
@@ -0,0 +1,8 @@
+---
+title: Hello Kasik!
+date: "2018-11-07T23:46:37.121Z"
+---
+
+Dupa i kitki
+
+Yeah, I didn't either.
